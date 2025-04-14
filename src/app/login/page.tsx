bd138187c5456810fcdc7808e3d213ecb72a00b9
Noel George C J <noelgeorge2004@gmail.com>
2025-04-14 15:23:20 +0530

    Login Page UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10 text-black">
+    <div className="flex bg-zinc-900 min-h-screen w-full items-center justify-center p-6 md:p-10 text-black">
       <div className="w-full max-w-4xl">
         <LoginForm />
       </div>
