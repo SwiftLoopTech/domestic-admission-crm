cbb59e5bd6af206130195efc52fe08f9cff8e303
Noel George C J <noelgeorge2004@gmail.com>
2025-04-16 14:33:46 +0530

    Dashboard parameters

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,6 +8,7 @@ import { ApplicationModal } from "@/components/application-modal";
 import { useAgentData } from "@/hooks/useAgentData";
 import { SelectContent, SelectTrigger, Select, SelectItem, SelectValue } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
+import { ArrowBigLeft, ChevronRight, GraduationCap, SearchIcon } from "lucide-react";
 
 export default function DashboardPage() {
   const { userRole, isLoading: isLoadingRole, error: roleError } = useUserRole();
@@ -46,80 +47,119 @@ export default function DashboardPage() {
           {/* Agent-specific content */}
           <div className="flex gap-4 mb-6">
             <Select>
-              <SelectTrigger>
+              <SelectTrigger className="border-zinc-400 hover:bg-zinc-200 transition focus-visible:ring-0">
                 <SelectValue placeholder="Select a college" />
               </SelectTrigger>
-              <SelectContent className="bg-white">
-                <SelectItem value="college1">College 1</SelectItem>
-                <SelectItem value="college2">College 2</SelectItem>
-                <SelectItem value="college3">College 3</SelectItem>
-                <SelectItem value="college4">College 4</SelectItem>
+              <SelectContent className="bg-white border-zinc-300">
+                <SelectItem value="college1" className="hover:bg-zinc-200">College 1</SelectItem>
+                <SelectItem value="college2" className="hover:bg-zinc-200">College 2</SelectItem>
+                <SelectItem value="college3" className="hover:bg-zinc-200">College 3</SelectItem>
+                <SelectItem value="college4" className="hover:bg-zinc-200">College 4</SelectItem>
               </SelectContent>
             </Select>
             <Select>
-              <SelectTrigger>
+              <SelectTrigger className="border-zinc-400 hover:bg-zinc-200 transition focus-visible:ring-0">
                 <SelectValue placeholder="Sub Agent" />
               </SelectTrigger>
-              <SelectContent className="bg-white">
-                <SelectItem value="college1">College 1</SelectItem>
-                <SelectItem value="college2">College 2</SelectItem>
-                <SelectItem value="college3">College 3</SelectItem>
-                <SelectItem value="college4">College 4</SelectItem>
+              <SelectContent className="bg-white border-zinc-300">
+                <SelectItem value="college1" className="hover:bg-zinc-200">College 1</SelectItem>
+                <SelectItem value="college2" className="hover:bg-zinc-200">College 2</SelectItem>
+                <SelectItem value="college3" className="hover:bg-zinc-200">College 3</SelectItem>
+                <SelectItem value="college4" className="hover:bg-zinc-200">College 4</SelectItem>
               </SelectContent>
             </Select>
             <Select>
-              <SelectTrigger>
+              <SelectTrigger className="border-zinc-400 hover:bg-zinc-200 transition focus-visible:ring-0">
                 <SelectValue placeholder="Select Year" />
               </SelectTrigger>
-              <SelectContent className="bg-white">
-                <SelectItem value="college1">Year 1</SelectItem>
-                <SelectItem value="college2">Year 2</SelectItem>
-                <SelectItem value="college3">Year 3</SelectItem>
-                <SelectItem value="college4">Year 4</SelectItem>
+              <SelectContent className="bg-white border-zinc-300">
+                <SelectItem value="college1" className="hover:bg-zinc-200">Year 1</SelectItem>
+                <SelectItem value="college2" className="hover:bg-zinc-200">Year 2</SelectItem>
+                <SelectItem value="college3" className="hover:bg-zinc-200">Year 3</SelectItem>
+                <SelectItem value="college4" className="hover:bg-zinc-200">Year 4</SelectItem>
               </SelectContent>
             </Select>
             <Select>
-              <SelectTrigger>
+              <SelectTrigger className="border-zinc-400 hover:bg-zinc-200 transition focus-visible:ring-0">
                 <SelectValue placeholder="Select Location" />
               </SelectTrigger>
-              <SelectContent className="bg-white">
-                <SelectItem value="college1">Kochi</SelectItem>
-                <SelectItem value="college2">Trivandrum</SelectItem>
-                <SelectItem value="college3">Kollam</SelectItem>
-                <SelectItem value="college4">Calicut</SelectItem>
+              <SelectContent className="bg-white border-zinc-300">
+                <SelectItem value="college1" className="hover:bg-zinc-200">Kochi</SelectItem>
+                <SelectItem value="college2" className="hover:bg-zinc-200">Trivandrum</SelectItem>
+                <SelectItem value="college3" className="hover:bg-zinc-200">Kollam</SelectItem>
+                <SelectItem value="college4" className="hover:bg-zinc-200">Calicut</SelectItem>
               </SelectContent>
             </Select>
             <Button className="bg-[#FFC11F] hover:bg-[#FFC11F] text-black font-medium rounded-md px-6 py-4">
               Apply Filter
             </Button>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <Card>
-              <CardHeader className="pb-2">
-                <CardTitle className="text-sm font-medium">Total Applications</CardTitle>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Total Applications</CardTitle>
               </CardHeader>
               <CardContent>
-                <p className="text-2xl font-bold">{stats.applicationsCount}</p>
+                <p className="text-4xl font-medium">{stats.applicationsCount}</p>
+              </CardContent>
+            </Card>
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Sub-Agents</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-4xl font-medium">{stats.subagentsCount}</p>
+              </CardContent>
+            </Card>
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Colleges</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-4xl font-medium">4</p>
+              </CardContent>
+            </Card>
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Offers</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-4xl font-medium">10</p>
+              </CardContent>
+            </Card>
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Payments</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-4xl font-medium">8</p>
+              </CardContent>
+            </Card>
+            <Card className="gap-3 h-fit w-2xs border-zinc-400">
+              <CardHeader className="">
+                <CardTitle className="text-sm text-zinc-700 font-medium">Non-Enrolments</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-4xl font-medium">2</p>
               </CardContent>
             </Card>
 
-            <Card>
-              <CardHeader className="pb-2">
-                <CardTitle className="text-sm font-medium">Sub-Agents</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-2xl font-bold">{stats.subagentsCount}</p>
-              </CardContent>
-            </Card>
-
-            <Card>
-              <CardHeader className="pb-2">
-                <CardTitle className="text-sm font-medium">Recent Activity</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-sm">No recent activity</p>
-              </CardContent>
-            </Card>
+          </div>
+          <div className="flex gap-5">
+            <div className="w-xs mt-12">
+              <div className="bg-[#FFC11F44] w-fit p-2 rounded-lg text-[#FFC11F]">
+                <SearchIcon width={24} height={24} />
+              </div>
+              <h6 className="text-xl mt-3 hover:underline cursor-pointer flex gap-1 items-start">Search Collges <ChevronRight width={20} /></h6>
+              <p className="text-sm mt-2 text-zinc-600">Explore colleges accepting applications through the platform. Filter courses, bulk upload courses and enrol new colleges.</p>
+            </div>
+            <div className="w-xs mt-12">
+              <div className="bg-[#FFC11F44] w-fit p-2 rounded-lg text-[#FFC11F]">
+                <GraduationCap width={24} height={24} />
+              </div>
+              <h6 className="text-xl mt-3 hover:underline cursor-pointer flex gap-1 items-start">Students<ChevronRight width={20} /></h6>
+              <p className="text-sm mt-2 text-zinc-600">Explore students applying through the platform.</p>
+            </div>
           </div>
         </div>
       ) : (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -131,7 +131,7 @@ export function Sidebar({ userRole }: SidebarProps) {
                         "flex items-center justify-between h-10 px-3 py-2 text-sm rounded-lg transition-all duration-200",
                         "hover:text-black",
                         isActive
-                          ? "text-black font-medium"
+                          ? "text-black font-medium bg-[#FFC11F]"
                           : "text-gray-600"
                       )}
                       onClick={() => setOpen(false)}
